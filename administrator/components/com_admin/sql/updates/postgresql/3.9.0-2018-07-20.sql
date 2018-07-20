47d35a1813ff7649f8eca33ceb70768e495c1b33
Nicola Galgano <extensions4joomla@gmail.com>
2018-07-20 10:29:01 +0200

    update job postgresql

---
+++ administrator/components/com_admin/sql/updates/postgresql/3.9.0-2018-07-20.sql
@@ -0,0 +1,3 @@
+INSERT INTO "#__extensions" ("extension_id", "package_id", "name", "type", "element", "folder", "client_id", "enabled", "access", "protected", "manifest_cache", "params", "custom_data", "system_data", "checked_out", "checked_out_time", "ordering", "state") VALUES
+(494, 0, 'plg_job_privacyconsent', 'plugin', 'privacyconsent', 'job', 0, 1, 1, 0, '', '{}', '', '', 0, '1970-01-01 00:00:00', 0, 0),
+(495, 0, 'plg_job_updatenotification', 'plugin', 'updatenotification', 'job', 0, 1, 1, 0, '', '{}', '', '', 0, '1970-01-01 00:00:00', 0, 0);
