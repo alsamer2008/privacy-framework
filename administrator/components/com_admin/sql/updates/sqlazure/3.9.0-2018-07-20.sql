b73efc5924098d1c51350c184dc994ab947346b3
Nicola Galgano <extensions4joomla@gmail.com>
2018-07-20 10:29:55 +0200

    update job mssql

---
+++ administrator/components/com_admin/sql/updates/sqlazure/3.9.0-2018-07-20.sql
@@ -0,0 +1,7 @@
+SET IDENTITY_INSERT #__extensions  ON;
+ 
+INSERT INTO "#__extensions" ("extension_id", "package_id", "name", "type", "element", "folder", "client_id", "enabled", "access", "protected", "manifest_cache", "params", "custom_data", "system_data", "checked_out", "checked_out_time", "ordering", "state") VALUES
+(494, 0, 'plg_job_privacyconsent', 'plugin', 'privacyconsent', 'job', 0, 1, 1, 0, '', '{}', '', '', 0, '1900-01-01 00:00:00', 0, 0),
+(495, 0, 'plg_job_updatenotification', 'plugin', 'updatenotification', 'job', 0, 1, 1, 0, '', '{}', '', '', 0, '1900-01-01 00:00:00', 0, 0);
+
+SET IDENTITY_INSERT #__extensions  OFF;
